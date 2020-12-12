d22763095d89c5ce1671aa4f4a21f45f11b03bf1
kgc0120 <senru0508@gmail.com>
2020-12-12 14:00:58 +0900

    jdbc sample

---
+++ jdbcsample/src/main/java/me/bumblebee/Application.java
@@ -0,0 +1,24 @@
+package me.bumblebee;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class Application {
+    // psvm -> main 메소드 자동 완성
+    public static void main(String[] args) throws SQLException{
+        String url = "jdbc:postgresql://localhost:5432/springdata";
+        String username = "gyucheol";
+        String password = "pass";
+
+        try(Connection connection = DriverManager.getConnection(url, username, password)) {
+              System.out.println("Connection created : " + connection);
+              String sql = "INSERT INTO ACCOUNT VALUES(1, 'gyucheol', 'pass');";
+              try(PreparedStatement statement = connection.prepareStatement(sql)) {
+                  statement.execute();
+              }
+
+        }
+    }
+}
