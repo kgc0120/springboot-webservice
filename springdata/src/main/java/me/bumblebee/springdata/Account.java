e821be9a761b27be42e62d01db7d7d75d7a7b4e8
kgc0120 <senru0508@gmail.com>
2020-12-13 16:21:55 +0900

    Entity type Mapping

--- springdata/src/main/java/me/bumblebee/springdata/Account.java
+++ springdata/src/main/java/me/bumblebee/springdata/Account.java
@@ -1,18 +1,31 @@
 package me.bumblebee.springdata;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.Date;
 
 @Entity
+// 특정 db에서는 user라는 테이블을 만들 수가 없다.(postgres)
 public class Account {
 
     @Id @GeneratedValue // @GeneratedValue 자동으로 생성되는 값을 사용하겠다라는 의미
     private Long id;
+    // long - primitive type, id가 0으로 세팅됨
+    // Long - wrapper type,
 
+    // properties에서 spring.jpa.hibernate.ddl-auto = update 해놓았다면 @Column 옵션을 변경해도 적용되지 않는다.
+    // 이미 칼럼이 만들어져있기 때문에
+    @Column(nullable = false, unique = true)
     private String username;
 
     private String password;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date created = new Date();
+
+    private String yes;
+
+    @Transient
+    private String no;
 
     public Long getId() {
         return id;
