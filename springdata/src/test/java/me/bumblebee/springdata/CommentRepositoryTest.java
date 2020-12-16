235b0eee04a6dbb6a9b7d85ead552cc1737db7cd
kgc0120 <senru0508@gmail.com>
2020-12-17 00:34:37 +0900

    make a query of Page

--- springdata/src/test/java/me/bumblebee/springdata/CommentRepositoryTest.java
+++ springdata/src/test/java/me/bumblebee/springdata/CommentRepositoryTest.java
@@ -4,6 +4,9 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 
 import java.util.List;
 import java.util.Optional;
@@ -19,20 +22,23 @@ public class CommentRepositoryTest {
     @Test
     @DisplayName("Repository 직접 정의해서 사용하기")
     void crud() {
-        this.createComment(100, "spring data jpa");
+        Comment spring_data_jpa = this.createComment(100, "spring data jpa");
         this.createComment(55, "hibernate spring");
 
-        List<Comment> comments = commentRepository.findByCommnetContainsIgnoreCaseOrderByLikeCountDesc("Spring");
-        assertEquals(comments.size(), 2);
+        PageRequest pageRequest = PageRequest.of(0, 10, Sort.by(Sort.Direction.DESC, "LikeCount"));
 
+        Page<Comment> comments = commentRepository.findByCommnetContainsIgnoreCase("Spring", pageRequest);
+        assertEquals(comments.getNumberOfElements(), 2);
+        assertEquals(comments.get().findFirst().get(), spring_data_jpa);
 
     }
 
-    private void createComment(int likeCount, String comment) {
+    private Comment createComment(int likeCount, String comment) {
         Comment newComment = new Comment();
         newComment.setLikeCount(likeCount);
         newComment.setCommnet(comment);
         commentRepository.save(newComment);
+        return newComment;
     }
 
 
