a584e01de3a58021c489a8a166cdb15d6d0670df
kgc0120 <senru0508@gmail.com>
2021-01-02 13:34:46 +0900

    scurity inMemory append user

--- inflearn_spring_security/demo/src/main/java/com/example/demo/config/SecurityConfig.java
+++ inflearn_spring_security/demo/src/main/java/com/example/demo/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.example.demo.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -19,5 +20,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.formLogin()
                 .and() // 메소드 체이닝 사용 안해도 상관없음
             .httpBasic();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("gyucheol").password("{noop}123").roles("USER").and()
+                // DB에 들어간 값이라고 생각하면 된다 {noop}123 {noop} prefix는 암호화를 안했다는 prefix
+                .withUser("admin").password("{noop}!@#").roles("ADMIN");
     }
 }
