2282b845533a82d88c8bb8d0a3d726c6fe5f0a3c
kgc0120 <senru0508@gmail.com>
2021-01-03 22:21:54 +0900

    password encoder

--- inflearn_spring_security/demo/src/main/java/com/example/demo/DemoApplication.java
+++ inflearn_spring_security/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -2,9 +2,19 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class DemoApplication {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        // {noop} prefix 사용할 필요없음
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
--- inflearn_spring_security/demo/src/main/java/com/example/demo/account/AccountService.java
+++ inflearn_spring_security/demo/src/main/java/com/example/demo/account/AccountService.java
@@ -5,6 +5,7 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,6 +14,9 @@ public class AccountService implements UserDetailsService {
     // TODO {noop}123  Spring Security 일정한 password 패턴이 들어가야한다.
     @Autowired
     AccountRepository accountRepository;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -29,7 +33,7 @@ public class AccountService implements UserDetailsService {
     }
 
     public Account createNew(Account account) {
-        account.encodePaaword();
+        account.encodePaaword(passwordEncoder);
         return this.accountRepository.save(account);
     }
 }
