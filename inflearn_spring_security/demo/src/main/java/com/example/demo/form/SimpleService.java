15fe6d419d96e9c0009258c50b2c476b1f240f95
kgc0120 <senru0508@gmail.com>
2021-01-06 22:14:14 +0900

    ThreadLocal

--- inflearn_spring_security/demo/src/main/java/com/example/demo/form/SimpleService.java
+++ inflearn_spring_security/demo/src/main/java/com/example/demo/form/SimpleService.java
@@ -1,5 +1,7 @@
 package com.example.demo.form;
 
+import com.example.demo.account.Account;
+import com.example.demo.account.AccountContext;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -12,12 +14,15 @@ public class SimpleService {
 
 
     public void dashboard() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        Object principal = authentication.getPrincipal(); //principal -> userDetail 사용자 정보 담겨져있다.
-        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();// 사용자 권한을 나타낸다. user일수도있고 admin일수도 있고
-        Object credentials = authentication.getCredentials();
-        boolean authenticated = authentication.isAuthenticated(); // 인증된 사용자냐
+//        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+//        Object principal = authentication.getPrincipal(); //principal -> userDetail 사용자 정보 담겨져있다.
+//        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();// 사용자 권한을 나타낸다. user일수도있고 admin일수도 있고
+//        Object credentials = authentication.getCredentials();
+//        boolean authenticated = authentication.isAuthenticated(); // 인증된 사용자냐
 
+        Account account = AccountContext.getAccount();
+        System.out.println("==============");
+        System.out.println(account.getUsername());
 
     }
 }
