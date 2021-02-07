ea1bd791abdb1577be706d09e1c5673cf710415e
kgc0120 <senru0508@gmail.com>
2021-02-08 00:34:54 +0900

    Delete inflearn_spring_security directory

--- inflearn_spring_security/demo/src/main/java/com/example/demo/account/AccountRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.account;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AccountRepository extends JpaRepository<Account, Integer> {
-    Account findByUsername(String username);
-}
--- inflearn_spring_security/demo/src/main/java/com/example/demo/config/MethodSecurity.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-
-@Configuration
-@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true, jsr250Enabled = true)
-public class MethodSecurity {
-}
