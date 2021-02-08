5fe1a330ce6f89246a9d5fa5aeae0619cb8da1de
kgc0120 <senru0508@gmail.com>
2021-02-08 23:33:34 +0900

    create Posts domain commit

---
+++ springboot-webservice/src/main/java/com/book/springboot/domain/posts/Posts.java
@@ -0,0 +1,32 @@
+package com.book.springboot.domain.posts;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor // 기본 생성자 자동 추가
+@Entity
+public class Posts {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // auto increment
+    private Long id;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+    @Builder // 생성자 상단에 선언 시 생성자에 포함된 필드만 빌더에 포함
+    public Posts(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+}
